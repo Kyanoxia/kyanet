bebc39dc35e7fd4c564e2a98fa0c8ff091dc7792
Kyanoxia <spektaclefr@gmail.com>
2024-07-12 17:32:30 -0700

    commit for cfp to test embed positions

--- src/components/Redirect.tsx
+++ src/components/Redirect.tsx
@@ -1,11 +1,24 @@
 import { useEffect } from "react"
+import { Helmet } from "react-helmet"
+
+import logo from "../assets/Kyanoxia.svg"
 
 const Redirect = ({ url }: { url: string }) => {
     useEffect(() => {
         window.location.href = url;
     }, [url]);
     
-    return <div className="landing min-h-screen flex flex-row justify-center items-center"><h1>Redirecting...</h1></div>
+    return (
+        <div className="landing min-h-screen flex flex-row justify-center items-center">
+            <Helmet>
+                <meta name="Redirect Page..." content="If you're seeing this, Helmet works well but the redirection element is broken!  Contact Kyanoxia to fix it!" />
+                <meta property="og:title" content="This is the OpenGraph Title" />
+                <meta property="og:description" content="This is the OpenGraph Description" />
+                <meta property="og:image" content={logo} />
+            </Helmet>
+            <h1>Redirecting...</h1>
+        </div>
+    )
 }
 
 export default Redirect
