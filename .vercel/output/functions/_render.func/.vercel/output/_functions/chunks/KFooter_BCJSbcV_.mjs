2bb5b87b6659366fbc321855d2bde007ad39e225
Kyanoxia <61282566+Kyanoxia@users.noreply.github.com>
2024-10-04 16:11:10 -0400

    Delete .vercel/output directory

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/KFooter_BCJSbcV_.mjs
+++
@@ -1,9 +0,0 @@
-import { c as createComponent, r as renderTemplate, m as maybeRenderHead } from './astro/server_DZrFQFCy.mjs';
-import 'kleur/colors';
-import 'clsx';
-
-const $$KFooter = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate`${maybeRenderHead()}<div class="kfooter" style="bottom: 0px; left: 0px; right: 0px; background-color: #1b1b1b; padding: 20px 20px; text-align: left;"> <sub style="color: #5b5b5b;">Copyright 2024 © Kyanoxia</sub> </div>`;
-}, "C:/Users/Kyanoxia/Documents/Developer/kyanet-astro/src/components/KFooter.astro", void 0);
-
-export { $$KFooter as $ };
