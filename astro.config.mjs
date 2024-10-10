fbabf5c6132936042eebea988e876d6280813248
Kyanoxia <spektaclefr@gmail.com>
2024-10-10 16:11:52 -0400

    Automatic Sitemap Integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kyanoxia.com',
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [sitemap({
+    filter: (page) => page !== 'https://kyanoxia.com/template/',
+  })]
 });
