b17a7f996fc248bfa2154792a084814ebabacb44
Kyanoxia <spektaclefr@gmail.com>
2025-03-07 14:11:54 -0500

    Implement Whitewind Blog

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,6 +24,25 @@ export const onRequest = defineMiddleware (async (context, next) => {
 
         return Response.redirect(new URL(`https://discord.com/invite/${inv}`), 302)
     }
+    else if (/(?<=\/blog\/)./g.test(context.url.pathname))
+    {
+        const did = "did:plc:cocpebhxzqhh2vxsp4ivzdbr";
+        const rkey = context.url.pathname.replace(/\/blog\//g, '');
+        const res = await fetch(`https://blewit.us-west.host.bsky.network/xrpc/com.atproto.repo.getRecord?repo=${did}&collection=com.whtwnd.blog.entry&rkey=${rkey}`);
+        const data = await res.json();
+
+        if (data.hasOwnProperty("value"))
+        {
+            console.log(data.value.visibility);
+            if (data.value.visibility === 'author')
+            {
+                return next("/404");
+            }
+        }
+        else {
+            return next("/404");
+        }
+    }
     
     console.log("MIDDLEWARE FINISHED")
     return next();
